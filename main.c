cd46a8b3736f48aa8efad073251343db8d4b5a7a
yuuuniii49 <0409yoon@sookmyung.ac.kr>
2020-10-08 15:44:19 +0900

    02-2

--- main.c
+++ main.c
@@ -3,18 +3,18 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int sumTwo(int a,int b);
+int square(int n);
 
 int main(int argc, char *argv[]) {
-	int a=10,b=20;
+	int n=5;
 	int res;
 	
-	res=sumTwo(a,b);
+	res=square(n);
 	printf("%d",res);	
 	
 	return 0;
 }
 
-int sumTwo(int a, int b){
-	return(a+b);
+int square(int n){
+	return(n*n);
 }
