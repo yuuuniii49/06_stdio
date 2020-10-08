ffbd7a63e5d2018694e5d1c5da7c3e89ca1831ea
yuuuniii49 <0409yoon@sookmyung.ac.kr>
2020-10-08 16:04:08 +0900

    03

--- main.c
+++ main.c
@@ -3,19 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-
+int square(int a)
+{
+	return(a*a);
+}
 int main(int argc, char *argv[]) {
 
+		int a=2;
+	 	
+	 	a = square(a);
+		printf("a=%i\n",a);
+		
+		return 0;
 	
-	printf("get_max : %i\n",get_max(10,20));	
-	
-	return 0;
-}
-
-int get_max(int x, int y){
-	
-	if(x > y)
-		return x;
-	else
-		return y;
-}
+	}
